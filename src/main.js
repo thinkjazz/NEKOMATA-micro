4f932bebecff3943eb30dac6f8c470c2227dfc26
Lookrecius <bragin.boris@ya.ru>
2019-12-03 20:47:51 +0300

    added import dateFilter & Vue.fileter(date, dateFilter)

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import dateFilter from './filters/date.filter'
 import '../node_modules/materialize-css/dist/js/materialize'
 
 Vue.config.productionTip = false
+
+Vue.filter('date', dateFilter)
 
 new Vue({
   router,
