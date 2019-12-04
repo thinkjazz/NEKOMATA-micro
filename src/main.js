cb88ccc2b6e76fe0a0244aa4d461dfc8b6fb9381
lookrecius <bragin.boris@ya.ru>
2019-12-04 03:43:01 +0300

    Added import of vuelidate and init by Vue.use() in main.js

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 import Vue from 'vue'
+import Vuelidate from 'vuelidate'
 import App from './App.vue'
-import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import dateFilter from './filters/date.filter'
+import './registerServiceWorker'
 import '../node_modules/materialize-css/dist/js/materialize'
 
 Vue.config.productionTip = false
-
+Vue.use(Vuelidate)
 Vue.filter('date', dateFilter)
 
 new Vue({
