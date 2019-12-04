86ce3aadc74e196ccd52666a801d1733a6306fdf
lookrecius <bragin.boris@ya.ru>
2019-12-04 04:45:35 +0300

    Added validate conditions & validate action in register block Register.vue

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,42 +1,64 @@
 <template>
-  <form class="card auth-card">
+  <form class="card auth-card" @submit.prevent="submitHandler">
     <div class="card-content">
       <span class="card-title">Домашняя бухгалтерия</span>
-      <div class="input-field">
-        <input
-            id="email"
-            type="text"
-        >
-        <label for="email">Email</label>
-        <small class="helper-text invalid">Email</small>
-      </div>
-      <div class="input-field">
-        <input
-            id="password"
-            type="password"
-            class="validate"
-        >
-        <label for="password">Пароль</label>
-        <small class="helper-text invalid">Password</small>
-      </div>
+       <div class="input-field">
+      <input
+          id="email"
+          type="text"
+          v-model.trim="email"
+          :class="{invalid: ($v.email.$dirty && !$v.email.required) || ($v.email.$dirty && !$v.email.email)}"
+      >
+      <label for="email">Email</label>
+      <small
+      class="helper-text invalid"
+      v-if="$v.email.$dirty && !$v.email.required"
+
+      >Поле не должно быть пустым</small>
+         <small
+      class="helper-text invalid"
+      v-else-if="$v.email.$dirty && !$v.email.email"
+
+      >Введите корректный Email</small>
+    </div>
+    <div class="input-field">
+      <input
+          id="password"
+          type="password"
+          v-model.trim="password"
+          :class="{invalid: ($v.password.$dirty && !$v.password.required) || ($v.password.$dirty && !$v.password.minLength)}"
+
+      >
+      <label for="password">Пароль</label>
+      <small class="helper-text invalid"
+      v-if="$v.password.$dirty && !$v.password.required"
+      >Введите пароль</small>
+       <small class="helper-text invalid"
+      v-else-if="$v.password.$dirty && !$v.password.minLength"
+      >Пароль должен быть {{$v.password.$params.minLength.min}} символов. Сейчас он {{password.length}}</small>
+    </div>
       <div class="input-field">
         <input
             id="name"
             type="text"
-            class="validate"
+            v-model.trim="name"
+            class="{$v.name.$dirty && !$v.name.required}"
         >
         <label for="name">Имя</label>
-        <small class="helper-text invalid">Name</small>
+        <small
+        class="helper-text invalid"
+        v-if="$v.name.$dirty && !$v.name.required"
+        >Введите Ваше имя</small>
       </div>
       <p>
         <label>
-          <input type="checkbox" />
+          <input type="checkbox" v-model="agree"/>
           <span>С правилами согласен</span>
         </label>
       </p>
     </div>
     <div class="card-action">
-      <div>
+      <div class="button-block">
         <button
             class="btn waves-effect waves-light auth-submit"
             type="submit"
@@ -47,8 +69,43 @@
       </div>
       <p class="center">
         Уже есть аккаунт?
-        <router-link to="/">Войти!</router-link>
+        <router-link to="/Login">Войти!</router-link>
       </p>
     </div>
   </form>
 </template>
+<script>
+import { email, required, minLength } from 'vuelidate/lib/validators'
+
+export default {
+
+  name: 'register',
+  data: () => ({
+    email: '',
+    password: '',
+    name: '',
+    agree: false
+  }),
+  validations: {
+    email: { email, required },
+    password: { required, minLength: minLength(8) },
+    name: { required },
+    agree: { checked: v => true }
+  },
+  methods: {
+    submitHandler () {
+      if (this.$v.$invalid) {
+        this.$v.$touch()
+        return
+      }
+      const formData = {
+        email: this.email,
+        password: this.password,
+        name: this.name
+      }
+      console.log(formData)
+      this.$router.push('/')
+    }
+  }
+}
+</script>
