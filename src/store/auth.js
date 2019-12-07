1433028c20002c8052dfa360c73f5b0f3ac95fdc
lookrecius <bragin.boris@ya.ru>
2019-12-07 15:40:40 +0300

    Created actions:register() getUid logout() via firebase into auth.js

--- src/store/auth.js
+++ src/store/auth.js
@@ -5,6 +5,23 @@ export default {
       try {
         await firebase.auth().signInWithEmailAndPassword(email, password)
       } catch (e) { throw e }
+    },
+    async register ({ dispatch }, { email, password, name }) {
+      try {
+        await firebase.auth().createUserWithEmailAndPassword(email, password)
+        let uid = await dispatch('getUid')
+        await firebase.database().ref(`/users/${uid}/info`).set({
+          bill: 10000,
+          name: name
+        })
+      } catch (e) { throw e }
+    },
+    getUid () {
+      let user = firebase.auth().currentUser
+      return user ? user.uid : null
+    },
+    async logout () {
+      await firebase.auth().signOut()
     }
   }
 }
