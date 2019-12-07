ea6f0b8ca116cbdf5deb0c0ef743ae0961c435da
lookrecius <bragin.boris@ya.ru>
2019-12-07 03:38:54 +0300

    Added and initialize function of  firebase mpm packege into main.js

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import router from './router'
 import store from './store'
 import dateFilter from './filters/date.filter'
 import messagePlugin from './utils/message.plugin'
+import firebase from '../node_modules/firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
 import './registerServiceWorker'
 import '../node_modules/materialize-css/dist/js/materialize'
 
@@ -12,9 +15,22 @@ Vue.config.productionTip = false
 Vue.use(messagePlugin)
 Vue.use(Vuelidate)
 Vue.filter('date', dateFilter)
-
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+firebase.initializeApp({
+  apiKey: 'AIzaSyABRVCmMqILVaMVlGB_Cmf39E0diIphhzI',
+  authDomain: 'vuecrm-2e568.firebaseapp.com',
+  databaseURL: 'https://vuecrm-2e568.firebaseio.com',
+  projectId: 'vuecrm-2e568',
+  storageBucket: 'vuecrm-2e568.appspot.com',
+  messagingSenderId: '1007134230193',
+  appId: '1:1007134230193:web:5ae1d16df4b7dc0c11c490'
+})
+let app
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
