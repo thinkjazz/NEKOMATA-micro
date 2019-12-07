7edc4657c4e740148c7a2536d795c39130c78330
lookrecius <bragin.boris@ya.ru>
2019-12-07 04:19:35 +0300

    Added import  auth and auto to modules to store/index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import auth from './auth'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
@@ -11,5 +11,6 @@ export default new Vuex.Store({
   actions: {
   },
   modules: {
+    auth
   }
 })
