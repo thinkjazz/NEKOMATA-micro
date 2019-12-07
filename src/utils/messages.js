cae53e63c61cc4b041287d462b0e289725542330
lookrecius <bragin.boris@ya.ru>
2019-12-07 20:47:38 +0300

    Added messages

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,3 +1,7 @@
 export default {
-  'logout': 'Вы вышли из системы'
+  'logout': 'Прощай %юзернейм%',
+  'auth/user-not-found': 'Такого %username% - не существует',
+  'auth/wrong-password': 'Неправильный пароль, %username%',
+  'auth/email-already-in-use': 'Email уже занят'
+
 }
