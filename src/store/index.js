a81984c90e213736c7c90cb6bb6646f5e6170803
lookrecius <bragin.boris@ya.ru>
2019-12-07 18:46:29 +0300

    Added mutation: setError() clearError() into store/index.js

--- src/store/index.js
+++ src/store/index.js
@@ -5,9 +5,17 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    error: null
   },
   mutations: {
+    setError (state, error) {
+      state.error = error
+    },
+    clearError (state) {
+      state.error = null
+    }
   },
+  getters: { error: (s) => s.error },
   actions: {
   },
   modules: {
