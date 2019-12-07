c22329f6339ba6ff330c8885643b29f66b22f3d6
lookrecius <bragin.boris@ya.ru>
2019-12-07 04:21:28 +0300

    Change methods to async submitHandler and await dispath login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -74,7 +74,7 @@ export default {
     }
   },
   methods: {
-    submitHandler () {
+    async submitHandler () {
       if (this.$v.$invalid) {
         this.$v.$touch()
         return
@@ -83,8 +83,10 @@ export default {
         email: this.email,
         password: this.password
       }
-      console.log(formData)
-      this.$router.push('/')
+      try {
+        await this.$store.dispatch('login', formData)
+        this.$router.push('/')
+      } catch (e) {}
     }
   }
 }
