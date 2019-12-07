88151035d02e85c60955ff0e47309704f3e64bff
lookrecius <bragin.boris@ya.ru>
2019-12-07 03:10:47 +0300

    Added bind message.plugin with Login.vue via mounted()

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -57,6 +57,7 @@
 </template>
 <script>
 import { email, required, minLength } from 'vuelidate/lib/validators'
+import messages from '../utils/messages'
 export default {
   name: 'login',
   data: () => ({
@@ -66,6 +67,11 @@ export default {
   validations: {
     email: { email, required },
     password: { required, minLength: minLength(8) }
+  },
+  mounted () {
+    if (messages[this.$route.query.message]) {
+      this.$message(messages[this.$route.query.message])
+    }
   },
   methods: {
     submitHandler () {
