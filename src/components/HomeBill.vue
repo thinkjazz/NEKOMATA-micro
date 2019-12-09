e991159ef4e5213828bb39cd5865ae2ea80cdf3b
lookrecius <bragin.boris@ya.ru>
2019-12-10 02:19:36 +0300

    Added new components HomeBill.vue & HomeCurrency.vue

---
+++ src/components/HomeBill.vue
@@ -0,0 +1,12 @@
+<template>
+<div class="col s12 m6 l4">
+    <div class="card grey lighten-5 bill-card">
+    <div class="card-content black-text">
+        <span class="card-title">Счет в валюте</span>
+        <p class="currency-line">
+        <span>12.0 Р</span>
+        </p>
+    </div>
+    </div>
+</div>
+</template>
---
+++ src/components/HomeCurrency.vue
@@ -0,0 +1,27 @@
+<template>
+<div class="col s12 m6 l8">
+    <div class="card grey lighten-5 bill-card">
+    <div class="card-content black-text">
+        <div class="card-header">
+        <span class="card-title">Курс валют</span>
+        </div>
+        <table>
+        <thead>
+        <tr>
+            <th>Валюта</th>
+            <th>Курс</th>
+            <th>Дата</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>руб</td>
+            <td>12121</td>
+            <td>12.12.12</td>
+        </tr>
+        </tbody>
+        </table>
+    </div>
+    </div>
+</div>
+</template>
