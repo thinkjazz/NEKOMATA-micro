928918f4cb47c48dd1fc198af7c60cff6da7f4b1
lookrecius <bragin.boris@ya.ru>
2019-12-10 02:22:02 +0300

    Deconstruct two blocks and moved to components/HomeBill.vue HomeCurrency.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,46 +7,18 @@
     </button>
   </div>
  <div class="row">
-    <div class="col s12 m6 l4">
-      <div class="card grey lighten-5 bill-card">
-        <div class="card-content black-text">
-          <span class="card-title">Счет в валюте</span>
-          <p class="currency-line">
-            <span>12.0 Р</span>
-          </p>
-        </div>
-      </div>
-    </div>
-    <div class="col s12 m6 l8">
-      <div class="card grey lighten-5 bill-card">
-        <div class="card-content black-text">
-          <div class="card-header">
-            <span class="card-title">Курс валют</span>
-          </div>
-          <table>
-            <thead>
-            <tr>
-              <th>Валюта</th>
-              <th>Курс</th>
-              <th>Дата</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr>
-              <td>руб</td>
-              <td>12121</td>
-              <td>12.12.12</td>
-            </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
-    </div>
+ <HomeBill />
+ <HomeCurrency />
   </div>
 </div>
 </template>
 <script>
+import HomeBill from '@/components/HomeBill'
+import HomeCurrency from '@/components/HomeCurrency'
 export default {
-  name: 'home'
+  name: 'home',
+  components: {
+    HomeBill, HomeCurrency
+  }
 }
 </script>
