e8c6cef9986db229271cbad83b736fbad38f7f2b
lookrecius <bragin.boris@ya.ru>
2019-12-14 17:15:27 +0300

    Added loading component into template in MainLayout.vue

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="app-main-layout">
+<div>
+    <Loader v-if="loading"/>
+  <div class="app-main-layout" v-else>
 
     <Navbar @click="isOpen = !isOpen"/>
 
@@ -16,6 +18,7 @@
       </router-link>
     </div>
   </div>
+</div>
 </template>
 <script>
 import Navbar from '../components/app/Navbar'
@@ -24,12 +27,14 @@ import Sidebar from '../components/app/Sidebar'
 export default {
   name: 'main-layout',
   data: () => ({
-    isOpen: true
+    isOpen: true,
+    loading: true
   }),
   async mounted () {
     if (!Object.keys(this.$store.getters.info).length) {
       await this.$store.dispatch('fetchInfo')
     }
+    this.loading = false
   },
   components: {
     Navbar,
