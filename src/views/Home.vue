579ec5efff4ec1f74cc8f60e11b9435a952006d1
lookrecius <bragin.boris@ya.ru>
2019-12-14 11:37:04 +0300

    Added componen in views/Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,17 +6,35 @@
       <i class="material-icons">refresh</i>
     </button>
   </div>
- <div class="row">
- <HomeBill />
+  <Loader v-if="loading"/>
+ <div v-else class="row">
+    <HomeBill
+    :rates="currency.rates"
+    />
  <HomeCurrency />
   </div>
 </div>
 </template>
 <script>
-import HomeBill from '@/components/HomeBill'
-import HomeCurrency from '@/components/HomeCurrency'
+import HomeBill from '../components/HomeBill'
+import HomeCurrency from '../components/HomeCurrency'
 export default {
   name: 'home',
+  data: () => ({
+    loading: true,
+    currency: null
+  }),
+  async mounted () {
+    this.currency = await this.$store.dispatch('fetchCurrency')
+    this.loading = false
+  },
+  methods: {
+    async refresh () {
+      this.loading = true
+      this.currency = await this.$store.dispatch('fetchCurrency')
+      this.loading = false
+    }
+  },
   components: {
     HomeBill, HomeCurrency
   }
