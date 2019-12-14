7ccec3fceb540efa9f5a4a9a2434470ca9141125
lookrecius <bragin.boris@ya.ru>
2019-12-14 11:36:04 +0300

    Added components of currency and bill in HomeBill.vue

--- src/components/HomeBill.vue
+++ src/components/HomeBill.vue
@@ -3,10 +3,31 @@
     <div class="card grey lighten-5 bill-card">
     <div class="card-content black-text">
         <span class="card-title">Счет в валюте</span>
-        <p class="currency-line">
-        <span>12.0 Р</span>
+        <p
+        v-for="cur of currencies"
+        :key ="cur"
+        class="currency-line">
+        <span>{{ getCurrency(cur) }}</span>
         </p>
     </div>
     </div>
 </div>
 </template>
+<script>
+export default {
+  props: ['rates'],
+  data: () => ({
+    currencies: ['RUB', 'USD', 'EUR']
+  }),
+  computed: {
+    base () {
+      return this.$store.getters.info.bill / (this.rates['RUB'] / this.rates['EUR'])
+    }
+  },
+  methods: {
+    getCurrency (currency) {
+      return Math.floor(this.base * this.rates[currency])
+    }
+  }
+}
+</script>
