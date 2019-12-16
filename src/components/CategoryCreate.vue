ab4163ea0ce54af99663498c61ccc82959c7b55e
lookrecius <bragin.boris@ya.ru>
2019-12-17 01:39:03 +0300

    Fixed limit and title methods and added enmiter in CategoryCreate.vue

--- src/components/CategoryCreate.vue
+++ src/components/CategoryCreate.vue
@@ -24,15 +24,15 @@
               <input
                   id="limit"
                   type="number"
-                  v-model="limit"
-                  :class="{invalid: $v.title.$dirty && !$v.title.minValue}"
+                  v-model.number="limit"
+                  :class="{invalid: $v.limit.$dirty && !$v.limit.minValue}"
               >
               <label for="limit">Лимит</label>
               <span
-              v-if="$v.title.$dirty && !$v.title.minValue"
+              v-if="$v.limit.$dirty && !$v.limit.minValue"
               class="helper-text invalid"
               >
-              Минимальная величина
+              Минимальное значение {{$v.limit.$params.minValue.min}}
               </span>
             </div>
 
@@ -49,17 +49,31 @@ import { required, minValue } from '../../node_modules/vuelidate/lib/validators'
 export default {
   data: () => ({
     title: '',
-    limit: 1
+    limit: 100
   }),
   validations: {
     title: { required },
     limit: { minValue: minValue(100) }
   },
+  mounted () {
+    window.M.updateTextFields()
+  },
   methods: {
-    submitHandeler () {
+    async submitHandler () {
       if (this.$v.$invalid) {
-        return this.$v.touch()
+        return this.$v.$touch()
       }
+      try {
+        let category = await this.$store.dispatch('createCategory', {
+          title: this.title,
+          limit: this.limit
+        })
+        this.title = ''
+        this.limit = 100
+        this.$v.$reset()
+        this.$message('Категория была создана')
+        this.$emit('created', category)
+      } catch (error) {}
     }
   }
 }
