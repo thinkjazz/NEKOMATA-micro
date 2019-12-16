78ee605bc4206e86b8aaaadb606aa801a7de38c1
lookrecius <bragin.boris@ya.ru>
2019-12-17 01:43:45 +0300

    Created: addNewCategory(category) and @created props

--- src/views/Categories.vue
+++ src/views/Categories.vue
@@ -5,7 +5,7 @@
   </div>
   <section>
     <div class="row">
-    <CategoryCreate/>
+    <CategoryCreate @created="addNewCategory"/>
     <CategoryEdit />
     </div>
   </section>
@@ -14,10 +14,20 @@
 <script>
 import CategoryCreate from '../components/CategoryCreate'
 import CategoryEdit from '../components/CategoryEdit'
+
 export default {
   name: 'categories',
+  data: () => ({
+    categories: []
+  }),
   components: {
     CategoryCreate, CategoryEdit
+  },
+  methods: {
+    addNewCategory (category) {
+      this.categories.push(category)
+      console.log(this.categories)
+    }
   }
 }
 </script>
