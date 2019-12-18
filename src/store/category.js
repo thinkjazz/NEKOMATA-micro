4fa41568b935c9e67d61139b9fe225987a01c4d6
lookrecius <bragin.boris@ya.ru>
2019-12-18 03:07:28 +0300

    Added: updateCategory() & fetchCategory() into store/category.js

--- src/store/category.js
+++ src/store/category.js
@@ -1,7 +1,36 @@
 import firebase from '../../node_modules/firebase/app'
 export default {
   actions: {
-    async  createCategory ({ commit, dispatch }, { title, limit }) {
+    async fetchCategories ({ commit, dispatch }) {
+      try {
+        let uid = await dispatch('getUid')
+        let categories = await (await (firebase.database().ref('/users/' + uid + '/categories').once('value'))).val() || {}
+        return Object.keys(categories).map(key => ({ ...categories[key], id: key }))
+        // Above code makes that below code
+        // let categoryToArrayFromDb = []
+        // Object.keys(categories).forEach(key => {
+        //   categoryToArrayFromDb.push({
+        //     title: categories[key].title,
+        //     limit: categories[key].limit,
+        //     id: key
+        //   })
+        // })
+        // return categoryToArrayFromDb
+      } catch (error) {
+        commit('setError', error)
+        throw error
+      }
+    },
+    async updateCategory ({ commit, dispatch }, { title, limit, id }) {
+      try {
+        let uid = await dispatch('getUid')
+        await firebase.database().ref('/users/' + uid + '/categories').child(id).update({ title, limit })
+      } catch (error) {
+        commit('setError', error)
+        throw error
+      }
+    },
+    async createCategory ({ commit, dispatch }, { title, limit }) {
       try {
         let uid = await dispatch('getUid')
         let category = await firebase.database().ref('/users/' + uid + '/categories').push({ title, limit })
