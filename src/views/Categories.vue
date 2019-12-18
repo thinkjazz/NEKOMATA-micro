6996298ade7a3ea162b8e03347039cf6d1a085fb
lookrecius <bragin.boris@ya.ru>
2019-12-18 03:08:29 +0300

    Added: updateCategory and data

--- src/views/Categories.vue
+++ src/views/Categories.vue
@@ -4,9 +4,16 @@
     <h3>Категории</h3>
   </div>
   <section>
-    <div class="row">
+    <Loader v-if="loading" />
+    <div class="row" v-else>
     <CategoryCreate @created="addNewCategory"/>
-    <CategoryEdit />
+    <CategoryEdit
+    v-if="categories.length"
+    :categories="categories"
+    :key="categories.length + updateCount"
+    @updated ="updateCategories"
+    />
+    <p v-else class="center">Категории отсутствуют</p>
     </div>
   </section>
 </div>
@@ -18,15 +25,26 @@ import CategoryEdit from '../components/CategoryEdit'
 export default {
   name: 'categories',
   data: () => ({
-    categories: []
+    categories: [],
+    loading: true,
+    updateCount: 0
   }),
+  async mounted () {
+    this.categories = await this.$store.dispatch('fetchCategories')
+    this.loading = false
+  },
   components: {
     CategoryCreate, CategoryEdit
   },
   methods: {
     addNewCategory (category) {
       this.categories.push(category)
-      console.log(this.categories)
+    },
+    updateCategories (category) {
+      let idx = this.categories.findIndex(c => c.id === category.id)
+      this.categories[idx].title = category.title
+      this.categories[idx].limit = category.limit
+      this.updateCount++
     }
   }
 }
