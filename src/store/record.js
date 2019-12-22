a60b5e03845585dc570cff2d1c3d4cbd98672012
lookrecius <bragin.boris@ya.ru>
2019-12-22 16:46:48 +0300

    Adddd record page into record.js

--- src/store/record.js
+++ src/store/record.js
@@ -10,6 +10,16 @@ export default {
         commit('setError', error)
         throw error
       }
+    },
+    async fetchRecords ({ dispatch, commit }) {
+      try {
+        let uid = await dispatch('getUid')
+        let records = await (await (firebase.database().ref('/users/' + uid + '/records').once('value'))).val() || {}
+        return Object.keys(records).map(key => ({ ...records[key], id: key }))
+      } catch (error) {
+        commit('setError', error)
+        throw error
+      }
     }
   }
 }
