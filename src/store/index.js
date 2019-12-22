9960f08f0b469c19466506ff50bf7c19edf86168
lookrecius <bragin.boris@ya.ru>
2019-12-22 15:16:49 +0300

    Added: update module

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,7 @@ import Vuex from 'vuex'
 import auth from './auth'
 import info from './info'
 import category from './category'
+import record from './record'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
@@ -28,6 +29,6 @@ export default new Vuex.Store({
     error: state => state.error
   },
   modules: {
-    auth, info, category
+    auth, info, category, record
   }
 })
