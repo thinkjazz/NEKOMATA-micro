99cf503c9b2fff3b14118aa3d4a963bcffcc8d71
lookrecius <bragin.boris@ya.ru>
2019-12-29 08:47:30 +0300

    Added feature change a name

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -3,16 +3,30 @@
     <div class="page-title">
       <h3>Профиль</h3>
     </div>
-    <form class="form">
+    <form class="form" @submit.prevent="submitHandler">
       <div class="input-field">
         <input
             id="description"
             type="text"
+            v-model="name"
+            :class="{invalid: $v.name.dirty && !$v.name.required}"
         >
         <label for="description">Имя</label>
         <span
-              class="helper-text invalid">name</span>
+
+              class="helper-text invalid"
+             v-if="$v.name.dirty && !$v.name.required"
+              >Поле должно быть пустым</span>
       </div>
+       <!-- Switch -->
+  <div class="switch">
+    <label>
+      English
+      <input type="checkbox">
+      <span class="lever"></span>
+      Русский
+    </label>
+  </div>
       <button class="btn waves-effect waves-light" type="submit">
         Обновить
         <i class="material-icons right">details</i>
@@ -20,3 +34,44 @@
     </form>
   </div>
 </template>
+<script>
+import { mapGetters, mapActions } from 'vuex'
+import { required } from 'vuelidate/lib/validators'
+export default {
+  data: () => ({
+    name: ''
+  }),
+  validations: {
+    name: { required }
+  },
+  mounted () {
+    this.name = this.info.name
+    setTimeout(() => {
+      window.M.updateTextFields()
+    })
+  },
+  computed: {
+    ...mapGetters(['info'])
+  },
+  methods: {
+    ...mapActions(['updateInfo']),
+    async submitHandler () {
+      if (this.$v.$invalid) {
+        this.$v.$touch()
+        return
+      }
+
+      try {
+        await this.updateInfo({
+          name: this.name
+        })
+      } catch (e) {}
+    }
+  }
+}
+</script>
+<style scoped>
+.switch {
+    margin-bottom: 2rem;
+}
+</style>
