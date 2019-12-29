6c94d1d489f29c82be0b86a09d0039ae15f8f62b
lookrecius <bragin.boris@ya.ru>
2019-12-29 06:41:02 +0300

    Change sryle Navber.vue

--- src/components/app/Navbar.vue
+++ src/components/app/Navbar.vue
@@ -19,13 +19,13 @@
             <ul id="dropdown" class="dropdown-content">
               <li>
                 <router-link to="/profile" class="black-text">
-                  <i class="material-icons">account_circle</i>Профиль
+                  <i class="material-icons">perm_contact_calendar</i>Профиль
                 </router-link>
               </li>
               <li class="divider" tabindex="-1"></li>
               <li>
                 <a href="#" class="black-text" @click.prevent="logout">
-                  <i class="material-icons">assignment_return</i>Выйти
+                  <i class="material-icons">exit_to_app</i>Выйти
                 </a>
               </li>
             </ul>
