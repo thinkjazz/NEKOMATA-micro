603840d7ad06bc3a22d8374639a81bae831bc73a
lookrecius <bragin.boris@ya.ru>
2019-12-29 06:45:06 +0300

    Added components to Detail.vue

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,24 +1,48 @@
 <template>
   <div>
-    <div>
+      <Loader v-if="loading"/>
+    <div v-else-if="record">
       <div class="breadcrumb-wrap">
-        <a href="/history" class="breadcrumb">История</a>
-        <a class="breadcrumb">
-          Расход
+        <router-link to="/history" class="breadcrumb">История</router-link>
+        <a @click.prevent class="breadcrumb">
+          {{record.type === 'income' ? 'Доход' : 'Расход'}}
         </a>
       </div>
       <div class="row">
         <div class="col s12 m6">
-          <div class="card red">
+          <div class="card"
+          :class="{'red' : record.type === 'outcome',
+          'green' : record.type === 'income'}"
+          >
             <div class="card-content white-text">
-              <p>Описание:</p>
-              <p>Сумма:</p>
-              <p>Категория:</p>
-              <small>12.12.12</small>
+              <p>Описание: {{record.description}}</p>
+              <p>Сумма: {{record.amount | currency}}</p>
+              <p>Категория: {{record.categoryName}}</p>
+                <small>{{record.date | date('datetime')}}</small>
             </div>
           </div>
         </div>
       </div>
     </div>
+    <p class="center" v-else>Запись с id={{$route.params.id}} не найдена</p>
   </div>
 </template>
+<script>
+export default {
+  name: 'detail',
+  data: () => ({
+    record: null,
+    loading: true
+  }),
+  async mounted () {
+    let id = this.$route.params.id
+    let record = await this.$store.dispatch('fetchRecordById', id)
+    let category = await this.$store.dispatch('fetchCategoryById', record.categoryId)
+    this.record = {
+      ...record,
+      categoryName: category.title
+    }
+    this.loading = false
+  }
+}
+</script>
