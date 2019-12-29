db0d0b0d364c30325f7af076a53bb63c37d1f580
lookrecius <bragin.boris@ya.ru>
2019-12-29 08:13:00 +0300

    Added chart.js vuechartjs in Histtory

--- src/views/History.vue
+++ src/views/History.vue
@@ -4,7 +4,7 @@
       <h3>История записей</h3>
     </div>
     <div class="history-chart">
-      <canvas></canvas>
+      <canvas ref='canvas'></canvas>
     </div>
     <Loader  v-if="loading"/>
     <p
@@ -30,9 +30,11 @@
 <script>
 import paginationMixin from './../mixins/pagination.mixin'
 import HistoryTable from './../components/HistoryTable'
+import { Pie } from 'vue-chartjs'
 
 export default {
   name: 'history',
+  extends: Pie,
   mixins: [paginationMixin],
   data: () => ({
     loading: true,
@@ -51,6 +53,37 @@ export default {
         typeText: record.type === 'income' ? 'Доход' : 'Расход'
       }
     }))
+    this.renderChart({
+      labels: categories.map(c => c.title),
+      datasets: [{
+        label: 'Расходы по категориям',
+        data: categories.map(c => {
+          return this.records.reduce((total, r) => {
+            if (r.categoryId === c.id && r.type === 'outcome') {
+              parseInt(total += r.amount)
+            }
+            return total
+          }, 0)
+        }),
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }]
+    })
     this.loading = false
   },
   components: {
