52e86b381e8d2c88ae1cccd2f11207ea379e1bf3
lookrecius <bragin.boris@ya.ru>
2019-12-29 06:45:52 +0300

    Added components to and mounted ()  History.vue

--- src/views/History.vue
+++ src/views/History.vue
@@ -6,35 +6,44 @@
     <div class="history-chart">
       <canvas></canvas>
     </div>
-    <section>
-      <table>
-        <thead>
-        <tr>
-          <th>#</th>
-          <th>Сумма</th>
-          <th>Дата</th>
-          <th>Категория</th>
-          <th>Тип</th>
-          <th>Открыть</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-          <td>1</td>
-          <td>1212</td>
-          <td>12.12.32</td>
-          <td>name</td>
-          <td>
-            <span class="white-text badge red">Расход</span>
-          </td>
-          <td>
-            <button class="btn-small btn">
-              <i class="material-icons">open_in_new</i>
-            </button>
-          </td>
-        </tr>
-        </tbody>
-      </table>
+    <Loader  v-if="loading"/>
+    <p
+    class="center"
+    v-else-if="!records.length"
+    >Записей пока нет
+<router-link to="/record" >Добавьте первую запись</router-link>
+    </p>
+    <section v-else>
+    <HistoryTable :records="records" />
     </section>
   </div>
 </template>
+<script>
+import HistoryTable from './../components/HistoryTable'
+export default {
+  name: 'history',
+  data: () => ({
+    loading: true,
+    records: [],
+    categories: []
+
+  }),
+  async mounted () {
+    // this.records = await this.$store.dispatch('fetchRecords')
+    let records = await this.$store.dispatch('fetchRecords')
+    this.categories = await this.$store.dispatch('fetchCategories')
+    this.records = records.map(record => {
+      return {
+        ...record,
+        categoryName: this.categories.find(c => c.id === record.categoryId).title,
+        typeClass: record.type === 'income' ? 'green' : 'red',
+        typeText: record.type === 'income' ? 'Доход' : 'Расход'
+      }
+    })
+    this.loading = false
+  },
+  components: {
+    HistoryTable
+  }
+}
+</script>
